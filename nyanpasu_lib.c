6757cfca4f02ace410e2dd5edd77825462bcd0f3
imperosol <56346771+imperosol@users.noreply.github.com>
2021-04-18 16:02:11 +0200

    Initial Commit

---
+++ nyanpasu_lib.c
@@ -0,0 +1,82 @@
+//
+// Created by thgir on 27/02/2021.
+//
+
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include "standard_functions.h"
+
+void *safe_malloc(size_t n) {
+    void *p = malloc(n);
+    if (p == NULL) {
+        fprintf(stderr, "Fatal: failed to allocate %zu bytes.\n", n);
+        exit(1);
+    }
+    return p;
+}
+
+FILE *open_file(const char *fileName, const char *mode) {
+    int err;
+    FILE *toOpen = NULL;
+    if ((err = fopen_s(&toOpen, fileName, mode))) {
+        fprintf(stderr, "cannot open file '%s': %s\n", fileName, strerror(err));
+        exit(1);
+    } else {
+        return toOpen;
+    }
+}
+
+
+void update_tm(struct tm* time, const int year, const int month, const int m_day,
+               const int hour, const int minute, const int seconds)
+{
+    time->tm_year += year;
+    time->tm_mon += month;
+    time->tm_mday += m_day;
+    time->tm_hour += hour;
+    time->tm_min += minute;
+    time->tm_sec += seconds;
+
+    mktime(time);
+}
+
+int input_word(char **word) {
+    char temp[40];
+    fgets(temp, 40, stdin);
+    *word = safe_malloc(sizeof(char) * strlen(temp) + 1);
+    strcpy(*word, temp);
+    printf("%s\n", *word);
+    return 0;
+}
+
+void clear_buffer(void) {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int power_of_two(int power) {
+    return 0b1 << power;
+}
+
+void swap(void *a, void *b, size_t len) {
+    unsigned char *p = a, *q = b, tmp;
+    for (size_t i = 0; i != len; ++i) {
+        tmp = p[i];
+        p[i] = q[i];
+        q[i] = tmp;
+    }
+}
+
+int str_to_int(const char * str) {
+    int number = 0;
+    /* security check */
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!is_digit(str[i]))
+            return 0;
+    }
+    /* conversion */
+    for (int i = strlen(str) - 1, power = 1; i >= 0; --i, power *= 10)
+        number += (str[i] - '0') * power;
+    return number;
+}
