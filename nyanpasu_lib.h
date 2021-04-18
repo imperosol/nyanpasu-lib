4c0e7864eabfec93946ac6b4549cfa910adaedfb
imperosol <56346771+imperosol@users.noreply.github.com>
2021-04-18 16:05:13 +0200

    initial commit

--- nyanpasu_lib.h
+++ nyanpasu_lib.h
@@ -5,23 +5,21 @@
 #ifndef NF05_PROJET_REMAKE_STANDARD_FUNCTIONS_H
 #define NF05_PROJET_REMAKE_STANDARD_FUNCTIONS_H
 
-
-#ifdef __unix
-#define fopen_s(pFile,filename,mode) ((*(pFile))=fopen((filename),(mode)))==NULL
-#endif
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 void * safe_malloc(size_t n);
+
 FILE* open_file(const char* fileName, const char* mode);
-void update_tm(struct tm* time, const int year, const int month, const int m_day,
-               const int hour, const int minute, const int seconds);
+
 int input_word(char **word);
+
 void clear_buffer(void);
-int power_of_two(int power);
+
+unsigned long power_of_two(int power);
+
 void swap(void *a, void *b, size_t len);
+
 int str_to_int(const char * str);
 
 #endif //NF05_PROJET_REMAKE_STANDARD_FUNCTIONS_H
